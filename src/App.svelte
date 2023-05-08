79ea1737f529527d1a50bcf88c4af795e40f70b7
Rafael-Sousa01 <126574084+Rafael-Sousa01@users.noreply.github.com>
2023-05-08 23:05:22 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,43 +5,71 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div class="board">
+    {#each board as cell, index}
+      <div class={`cell ${cell}`} on:click={() => makeMove(index)}>{cell}</div>
+    {/each}
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  {#if winner}
+    <p class="message">
+      {#if winner === 'tie'}
+        It's a tie!
+      {:else}
+       {winner} won!
+      {/if}
+    </p>
+  
+    <div class="reset" on:click={resetGame}>Reset Game</div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  .board {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 5px;
+  margin-bottom: 20px;
+}
+
+.cell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 4rem;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: #eee;
+  transition: background-color 0.2s;
+}
+
+.cell:hover {
+  background-color: #ddd;
+}
+
+.cell.X {
+  color: #f00;
+}
+
+.cell.O {
+  color: #00f;
+}
+
+.message {
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.reset {
+  padding: 10px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: #eee;
+  transition: background-color 0.2s;
+}
+
+.reset:hover {
+  background-color: #ddd;
+}
 </style>
